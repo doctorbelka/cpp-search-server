dadbc1af5f73c6c45d3433f15b58c631ba140b48
doctorbelka <avdbor.junior@mail.ru>
2023-01-23 20:33:32 +0600

    SearchServer 2 Sprint Fixed

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -6,10 +6,12 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const double EPSILON=1e-6;
 
 string ReadLine() {
     string s;
@@ -82,7 +84,7 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                 if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                 if (abs(lhs.relevance - rhs.relevance) < EPSILON) {
                      return lhs.rating > rhs.rating;
                  } else {
                      return lhs.relevance > rhs.relevance;
@@ -95,7 +97,7 @@ public:
     }
     
     vector<Document> FindTopDocuments(const string& raw_query) const{
-        return FindTopDocuments(raw_query,[](int document_id,DocumentStatus status,int rating){return status==DocumentStatus::ACTUAL;});
+        return FindTopDocuments(raw_query,DocumentStatus::ACTUAL);
     }
     
     vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const{
@@ -157,10 +159,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(ratings.begin(),ratings.end(),0);
         return rating_sum / static_cast<int>(ratings.size());
     }
 
